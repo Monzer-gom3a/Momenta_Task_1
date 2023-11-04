5d73228eaf8010a8b59c96edbb63eb705f434ffc
Monzer-gom3a <MonzerMohammed9955@gmail.com>
2023-11-04 16:38:44 +0200

    build project

---
+++ lib/main.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData.light(useMaterial3: true), home: const MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 7,
+        centerTitle: true,
+        title: const Text(
+          "Counter app (Task 1)",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              width: 300,
+              child: Text(
+                "This is first task and it's very Simple. just a counter.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.purple,
+                      blurRadius: 10,
+                    )
+                  ],
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  borderRadius: BorderRadius.circular(15)),
+              width: 280,
+              height: 300,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    child: Text(
+                      count.toString(),
+                      style: const TextStyle(fontSize: 55),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              count--;
+                            });
+                          },
+                          icon: const Icon(
+                            Icons.remove,
+                            size: 30,
+                          ),
+                        ),
+                      ),
+                      CircleAvatar(
+                        radius: 30,
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              count++;
+                            });
+                          },
+                          icon: const Icon(
+                            Icons.add,
+                            size: 30,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        onPressed: () {
+          setState(() {
+            count = 0;
+          });
+        },
+        child: const Text("Zero"),
+      ),
+    );
+  }
+}
